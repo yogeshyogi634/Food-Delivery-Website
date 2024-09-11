6e01c97966e4bcf45358e81bcf9209b4da1d6b7c
yogeshyogi634 <yogeshyogi634@gmail.com>
2024-09-12 00:10:41 +0530

    files added

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,7 +1,40 @@
 import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
 
-const ExploreMenu = () => {
-  return <div></div>;
+const ExploreMenu = ({ category, setCategory }) => {
+  return (
+    <div className="explore-menu" id="explore-menu">
+      <h1>Explore our menu</h1>
+      <p className="explore-menu-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        nihil, distinctio, alias et commodi hic illum dolores beatae eligendi
+        voluptatum magnam consequatur nesciunt.
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((list, index) => {
+          return (
+            <div
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === list.menu_name ? "All" : list.menu_name
+                )
+              }
+              key={index}
+              className="explore-menu-list-item"
+            >
+              <img
+                src={list.menu_image}
+                alt=""
+                className={category === list.menu_name ? "active" : ""}
+              />
+              <p>{list.menu_name}</p>
+            </div>
+          );
+        })}
+      </div>
+      <hr />
+    </div>
+  );
 };
 
 export default ExploreMenu;
---
+++ src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,47 @@
+import { useContext } from "react";
+import { assets } from "../../assets/assets";
+import "./FoodItem.css";
+import { StoreContext } from "../../context/StoreContext";
+
+const FoodItem = ({ id, name, description, price, image }) => {
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
+
+  return (
+    <div className="food-item">
+      <div className="food-item-img-container">
+        <img src={image} alt="" className="food-item-img" />
+        {!cartItems[id] ? (
+          <img
+            className="add"
+            onClick={() => addToCart(id)}
+            src={assets.add_icon_white}
+          ></img>
+        ) : (
+          <div className="food-item-counter">
+            <img
+              onClick={() => removeFromCart(id)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
+            {cartItems[id]}
+            <img
+              onClick={() => addToCart(id)}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
+        )}
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="" />
+        </div>
+        <div className="food-item-desc">{description}</div>
+        <div className="food-item-price">${price}</div>
+      </div>
+    </div>
+  );
+};
+
+export default FoodItem;
